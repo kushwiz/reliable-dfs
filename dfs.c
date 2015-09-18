a36fff9b98400369a5f770e954904903a50b9e1d
Kushal Bhandari <kushwiz@gmail.com>
2015-09-18 14:27:18 -0400

    Regex pattern match and extract

--- dfs.c
+++ dfs.c
@@ -4,7 +4,7 @@
 #include<regex.h>
 #include<errno.h>
 
-#define REGISTER "^REGISTER ^(.+) ([0-9]+)$"
+#define REGISTER_CMD_REGEX "^REGISTER (.+) ([0-9]+)$"
 
 int main(int argc, char **argv) {
 
@@ -12,31 +12,35 @@ int main(int argc, char **argv) {
   char userInput[100];
   int portNo;
   regex_t re;
-  regmatch_t rm[2];
+  regmatch_t rm[3];
 
   if(argc != 3) {
     printf("Usage:");
-    printf("./dfs <program> <port>");
+    printf("./dfs <type> <port>");
     return 0;
   } else {
     portNo = atoi(argv[2]);
     strcpy(programType, argv[1]);
     if((strcmp(programType, "server")!=0) && (strcmp(programType,"client")!=0)) {
       printf("Usage:");
-      printf("./dfs <program> <port>");     
+      printf("./dfs <type> <port>");     
       return 0;
     }
   }
 
-  if (regcomp(&re, REGISTER, REG_EXTENDED) != 0) {
-    fprintf(stderr, "Failed to compile regex '%s'\n", REGISTER);
+  if (regcomp(&re, REGISTER_CMD_REGEX, REG_EXTENDED) != 0) {
+    fprintf(stderr, "Failed to compile regex '%s'\n", REGISTER_CMD_REGEX);
     return EXIT_FAILURE;
   }
 
   strcpy(userInput, "REGISTER hello 12");
 
-  regexec(&re, userInput, 2, rm, 0);
-  printf("<<%d>>", rm[0].rm_eo);
+  if (regexec(&re, userInput, 3, rm, 0) == 0) {
+    printf("Text: <<%.*s>>\n", (int)(rm[1].rm_eo - rm[1].rm_so), userInput + rm[1].rm_so);
+    printf("Text: <<%.*s>>\n", (int)(rm[2].rm_eo - rm[2].rm_so), userInput + rm[2].rm_so);
+  } else {
+    printf("Invalid Command, See HELP");
+  }
 
   printf("\nStarting %s on port %d", programType, portNo);
   
